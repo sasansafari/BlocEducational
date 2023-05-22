59612a600ab5f876d06c25f780316c45ea8f2ebb
sasansafari <sasansafari19@gmail.com>
2023-05-22 18:02:50 +0330

    documnet

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
  
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:BlocAPi/bloc/app_events.dart';
@@ -17,14 +19,20 @@ class Home extends StatelessWidget {
         RepositoryProvider.of<Repository>(context)
         )..add(LoadArticleEvent()),
       child: Scaffold(
-        body:  BlocBuilder<ArticleBLoc,ArticleState>(builder: (context, state) {
+
+        body:  BlocBuilder<ArticleBLoc,ArticleState>(
+          builder: (context, state) {
+
           if (state is ArticleLoadingState) {
             return const Center(
               child: CircularProgressIndicator(),
             );
           }
+
+          //- اگر وضعیت `ArticleLoadingState` باشد، یعنی بلوک در حال بارگذاری مقالات است، یک نمایشگر `CircularProgressIndicator` که یک نشانگر پیشرفت است نمایش داده می‌شود. این نشانگر معمولاً برای نشان دادن اینکه عملیات بارگذاری در حال انجام است استفاده می‌شود.
           if (state is ArticleLoadedState) {
             List<Article> articlesList= state.articles;
+
             return ListView.builder(
               itemCount: articlesList.length,
               itemBuilder: (context, index) {
@@ -34,10 +42,21 @@ class Home extends StatelessWidget {
                 );
               },
             );
+
           }
 
+          //- اگر وضعیت `ArticleLoadedState` باشد، یعنی مقالات با موفقیت بارگذاری شده‌اند، لیستی از مقالات در یک `ListView` نمایش داده می‌شود. از `ListView.builder` استفاده می‌شود تا بتوان به طور پویا و به اندازه‌ی لازم عناصر را در لیست نمایش داد.
+          if (state is ArticleErrorState) {
+            String  error= state.error;
 
-          return Container();
+            return Center(
+              child: Text(error),
+            );
+            
+          }
+
+          //- در غیر این صورت، یعنی وضعیت دیگری به جز `ArticleLoadingState` و `ArticleLoadedState` است، یک `Container` خالی نمایش داده می‌شود.
+          return Text("no data");
         },)
     ));
   }
